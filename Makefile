7213fedd37d6aef182810c6ef70b3373fd54db43
Stepan Shi <ste-shi@mail.ru>
2021-02-15 20:31:15 +0300

    github actions

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ gendiff:
 	node bin/genDiff.js
 publish:
 	npm publish --dry-run
+make lint:
+	npx eslint .
--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
-//import version from '../../package.json';
-
 
 import main from '../index.js';
 
 main();
-
 
 // console.log('sooooo:');
 // if (options.format) console.log(`- ${options.format}`);
