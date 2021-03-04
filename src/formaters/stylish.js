4a399d564639c4cca58a7ce44889d197f6e2fd0b
Stepan Shi <ste-shi@mail.ru>
2021-03-04 19:51:18 +0300

    refactoring

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -9,23 +9,23 @@ const format = (data, replacer = ' ', spaceCount = 4) => {
     const bracketIndent = replacer.repeat((deeplvl - 1) * spaceCount);
 
     if (Array.isArray(obj)) {
-      const maped = obj.map((container) => {
-        if (container.type === 'changed') {
+      const maped = obj.map((current) => {
+        const changedContainerToStr = (container) => {
           const preState = `${indent}- ${container.key}: ${iterTree(container.value[0], currSpaceCount + spaceCount)}`;
           const postState = `${indent}+ ${container.key}: ${iterTree(container.value[1], currSpaceCount + spaceCount)}`;
           return `${preState}\n${postState}`;
-        }
-        const containerToStr = (container1) => {
-          const keyValue = `${container1.key}: ${iterTree(container1.value, currSpaceCount + spaceCount)}`;
+        };
+        const containerToStr = (container) => {
+          const keyValue = `${container.key}: ${iterTree(container.value, currSpaceCount + spaceCount)}`;
           const resultStr = {
             deleted: `${indent}- ${keyValue}`,
             new: `${indent}+ ${keyValue}`,
             obj: `${indent}  ${keyValue}`,
             same: `${indent}  ${keyValue}`,
           };
-          return resultStr[container1.type];
+          return resultStr[container.type];
         };
-        return containerToStr(container);
+        return (current.type === 'changed') ? changedContainerToStr(current) : containerToStr(current);
       });
       const resultStr = `{\n${maped.join('\n')}\n${bracketIndent}}`;
       return resultStr;
