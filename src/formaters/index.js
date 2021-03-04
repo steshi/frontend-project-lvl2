bf8dc9ffac975048a4173731355204013e80e67c
Stepan Shi <ste-shi@mail.ru>
2021-03-04 17:35:04 +0300

    refactoring

--- src/formaters/index.js
+++ src/formaters/index.js
@@ -3,21 +3,12 @@ import plain from './plain.js';
 import json from './json.js';
 
 const format = (ast, formatType) => {
-  if (formatType === 'stylish') {
-    const formatedTree = stylish(ast);
-    return formatedTree;
-  }
-  if (formatType === 'plain') {
-    const formatedTree = plain(ast);
-    return formatedTree;
-  }
-  if (formatType === 'json') {
-    const formatedTree = json(ast);
-    return formatedTree;
-  }
-
-  const message = `Err. ${formatType} - no such format.`;
-  return message;
+  const foramters = {
+    stylish: stylish(ast),
+    plain: plain(ast),
+    json: json(ast),
+  };
+  return foramters[formatType];
 };
 
 export default format;
