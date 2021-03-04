5fae2914df0f14526c84b34bf0740febab08f7f2
Stepan Shi <ste-shi@mail.ru>
2021-03-04 19:17:14 +0300

    refactoring

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -10,13 +10,6 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('buildDiff json files in not supported format', () => {
-  const file1Path = path.join(__dirname, '..', '__fixtures__', 'file1.json');
-  const file2Path = path.join(__dirname, '..', '__fixtures__', 'file2.json');
-  const expectedResult = '[any] format not supported';
-  const actualResult = genDiff(file1Path, file2Path, 'any');
-  expect(actualResult).toBe(expectedResult);
-});
 test('buildDiff json files in stylish format', () => {
   const file1Path = path.join(__dirname, '..', '__fixtures__', 'file1.json');
   const file2Path = path.join(__dirname, '..', '__fixtures__', 'file2.json');
@@ -43,5 +36,12 @@ test('buildDiff files in JSON format', () => {
   const file2Path = path.join(__dirname, '..', '__fixtures__', 'file2.yml');
   const expectedResult = readFile('resultJson.txt');
   const actualResult = genDiff(file1Path, file2Path, 'json');
+  expect(actualResult).toBe(expectedResult);
+});
+test('buildDiff files in not supported format', () => {
+  const file1Path = path.join(__dirname, '..', '__fixtures__', 'file1.json');
+  const file2Path = path.join(__dirname, '..', '__fixtures__', 'file2.json');
+  const expectedResult = '[any] format not supported';
+  const actualResult = genDiff(file1Path, file2Path, 'any');
   expect(actualResult).toBe(expectedResult);
 });
--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -5,28 +5,27 @@ const format = (data, replacer = ' ', spaceCount = 4) => {
     }
 
     const deeplvl = currSpaceCount / spaceCount;
-    const positiveIndent = `${replacer.repeat(currSpaceCount - 2)}+ `;
-    const negativeIndent = `${replacer.repeat(currSpaceCount - 2)}- `;
-    const neutralIndent = `${replacer.repeat(currSpaceCount - 2)}  `;
+    const indent = `${replacer.repeat(currSpaceCount - 2)}`;
     const bracketIndent = replacer.repeat((deeplvl - 1) * spaceCount);
 
     if (Array.isArray(obj)) {
       const maped = obj.map((container) => {
         if (container.type === 'changed') {
-          const preState = `${negativeIndent}${container.key}: ${iterTree(container.value[0], currSpaceCount + spaceCount)}`;
-          const postState = `${positiveIndent}${container.key}: ${iterTree(container.value[1], currSpaceCount + spaceCount)}`;
+          const preState = `${indent}- ${container.key}: ${iterTree(container.value[0], currSpaceCount + spaceCount)}`;
+          const postState = `${indent}+ ${container.key}: ${iterTree(container.value[1], currSpaceCount + spaceCount)}`;
           return `${preState}\n${postState}`;
         }
-        if (container.type === 'deleted') {
-          return `${negativeIndent}${container.key}: ${iterTree(container.value, currSpaceCount + spaceCount)}`;
-        }
-        if (container.type === 'new') {
-          return `${positiveIndent}${container.key}: ${iterTree(container.value, currSpaceCount + spaceCount)}`;
-        }
-        if (container.type !== 'obj') {
-          return `${neutralIndent}${container.key}: ${iterTree(container.value, currSpaceCount + spaceCount)}`;
-        }
-        return `${neutralIndent}${container.key}: ${iterTree(container.value, currSpaceCount + spaceCount)}`;
+        const containerToStr = (container1) => {
+          const keyValue = `${container1.key}: ${iterTree(container1.value, currSpaceCount + spaceCount)}`;
+          const resultStr = {
+            deleted: `${indent}- ${keyValue}`,
+            new: `${indent}+ ${keyValue}`,
+            obj: `${indent}  ${keyValue}`,
+            same: `${indent}  ${keyValue}`,
+          };
+          return resultStr[container1.type];
+        };
+        return containerToStr(container);
       });
       const resultStr = `{\n${maped.join('\n')}\n${bracketIndent}}`;
       return resultStr;
@@ -35,12 +34,11 @@ const format = (data, replacer = ' ', spaceCount = 4) => {
     const keys = Object.keys(obj);
     const constructor = keys.map((key) => {
       if (typeof obj[key] === 'object') {
-        return `${neutralIndent}${key}: ${iterTree(obj[key], currSpaceCount + spaceCount)}`;
+        return `${indent}  ${key}: ${iterTree(obj[key], currSpaceCount + spaceCount)}`;
       }
-      return `${neutralIndent}${key}: ${obj[key]}`;
+      return `${indent}  ${key}: ${obj[key]}`;
     });
     const str = constructor.join('\n');
-
     const resultStr = `{\n${str}\n${bracketIndent}}`;
     return resultStr;
   };
