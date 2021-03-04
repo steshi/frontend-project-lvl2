4a45b4c0455900b89c84a81793f314c8c7f88a3e
Stepan Shi <ste-shi@mail.ru>
2021-03-04 17:27:30 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 import parser from './src/parsers.js';
-import * as formaters from './src/formaters/index.js';
+import format from './src/formaters/index.js';
 import buildDiff from './src/buildDiff.js';
 
 const genDiff = (filepath1, filepath2, formatType) => {
   const content1 = parser(filepath1);
   const content2 = parser(filepath2);
   const ast = buildDiff(content1, content2);
-  if (formatType === 'stylish') {
-    const formatedTree = formaters.stylish(ast);
-    return formatedTree;
-  }
-  if (formatType === 'plain') {
-    const formatedTree = formaters.plain(ast);
-    return formatedTree;
-  }
-  if (formatType === 'json') {
-    const formatedTree = formaters.json(ast);
-    return formatedTree;
-  }
-
-  const message = `Err. ${formatType} - no such format.`;
-  return message;
+  return format(ast, formatType);
 };
 export default genDiff;
--- src/formaters/index.js
+++ src/formaters/index.js
@@ -2,4 +2,22 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-export { stylish, plain, json };
+const format = (ast, formatType) => {
+  if (formatType === 'stylish') {
+    const formatedTree = stylish(ast);
+    return formatedTree;
+  }
+  if (formatType === 'plain') {
+    const formatedTree = plain(ast);
+    return formatedTree;
+  }
+  if (formatType === 'json') {
+    const formatedTree = json(ast);
+    return formatedTree;
+  }
+
+  const message = `Err. ${formatType} - no such format.`;
+  return message;
+};
+
+export default format;
