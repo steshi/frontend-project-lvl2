46f88a9c71fb67310133f5ab4ac1defc7ca5ac3d
Stepan Shi <ste-shi@mail.ru>
2021-03-05 18:42:01 +0300

    passing hexlet-check

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,29 +1,16 @@
 import _ from 'lodash';
 
 const makeNode = (obj1, obj2, key) => {
-  // const node = {};
-  // node.name = key;
   if (obj1[key] === undefined) {
     return { name: key, type: 'new', value: obj2[key] };
-    // node.type = 'new';
-    // node.value = obj2[key];
   }
   if (obj2[key] === undefined) {
     return { name: key, type: 'deleted', value: obj1[key] };
-    // node.type = 'deleted';
-    // node.value = obj1[key];
   }
   if (obj1[key] === obj2[key]) {
     return { name: key, type: 'same', value: obj1[key] };
-    // node.type = 'same';
-    // node.value = obj1[key];
   }
-  // if (node.type === undefined) {
   return { name: key, type: 'changed', value: [obj1[key], obj2[key]] };
-  // node.type = 'changed';
-  // node.value = [obj1[key], obj2[key]];
-  // }
-  // return node;
 };
 
 const buildDiff = (obj1, obj2) => {
@@ -33,12 +20,8 @@ const buildDiff = (obj1, obj2) => {
   const uniqeKeys = Array.from(new Set(concatKeys));
   const sorted = _.orderBy(uniqeKeys);
   const result = sorted.reduce((acc, key) => {
-    const node = {};
-    node.name = key;
     if ((typeof obj1[key] === 'object') && (typeof obj2[key] === 'object')) {
-      node.type = 'obj';
-      node.value = buildDiff(obj1[key], obj2[key]);
-      return [...acc, node];
+      return [...acc, { name: key, type: 'obj', value: buildDiff(obj1[key], obj2[key]) }];
     }
     return [...acc, makeNode(obj1, obj2, key)];
   }, []);
